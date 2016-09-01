9af8ef2a99ca940616ca15ab1be65c09b5f225f1
sumanthjamadagni <jamadagni.sn@pg.com>
2016-09-01 09:30:30 -0400

    Add files via upload
    LJ_Isotherms.py edited slightly to calculate for both vapor and liquid sides.

--- LJ_Isotherms.py
+++ LJ_Isotherms.py
@@ -82,35 +82,31 @@ PlotFreq = 1
 
 cr_guess = None
 nrho_vap = len(rho_vap)
-rho_converged_Vap  = []
-Pressures_Vap = []
-Compressibility_Vap = []
-ExcessEntropy_Vap = []
-
 nrho_liq = len(rho_liq)
-rho_converged_Liq  = []
-Pressures_Liq = []
-Compressibility_Liq = []
-ExcessEntropy_Liq = []
 
-#rho = np.append(rho_vap)
+rho_converged  = []
+Pressures = []
+Compressibility = []
+ExcessEntropy = []
 
+rho_all = np.append(rho_vap, rho_liq)
+nrho = len(rho_all)
 cr_guess = None
-for irho in range(nrho_liq):
-    rho = rho_liq[irho]    
+for irho in range(nrho):
+    rho = rho_all[irho]    
     Flag, hr, cr, er, hk, Sk = RHNC.OZSolver_RHNC_Iterative(r, k, Ur, Ur_ref, rho, kT=T, maxiter=10000, w_old_start=0.50,w_old_max=0.99,tol=1e-8, cr_guess=cr_guess)    
     if Flag == 0:
         cr_guess = cr #for next density
         P = PP_Functions.Calc_Pvirial(r,hr+1,Ur,rho,kT=T, rmin=0.25)
         print " P = ", P 
-        Pressures_Liq.append(P)
-        rho_converged_Liq.append(rho)
+        Pressures.append(P)
+        rho_converged.append(rho)
         kappa = PP_Functions.CalculateKappa(1+rho*hk, rho, kT=T)
-        Compressibility_Liq.append(kappa)
+        Compressibility.append(kappa)
         
         nr = PP_Functions.CoordinationNumber(r,hr+1,rho)
         s2 = PP_Functions.s2(r,hr+1,rho)
-        ExcessEntropy_Liq.append(s2)
+        ExcessEntropy.append(s2)
                         
         if irho % PlotFreq == 0:
             Fig1.plot(r, hr+1, linewidth=2, label='$\\rho = $' + str(rho))
@@ -121,10 +117,10 @@ for irho in range(nrho_liq):
         
 
 
-rho_converged_Liq = np.array(rho_converged_Liq)
-P_gas = rho_converged_Liq * T + rho_converged_Liq**2 * T * B2 
+rho_converged = np.array(rho_converged)
+P_gas = rho_converged * T + rho_converged**2 * T * B2 
 
-P_compressibility = PP_Functions.Calc_Pcompressibility(rho_converged_Liq, Compressibility_Liq, opt='cum',kT=T, B2=B2)
+P_compressibility = PP_Functions.Calc_Pcompressibility(rho_converged, Compressibility, opt='cum',kT=T, B2=B2)
 
 Fig1.legend(loc='upper right', ncol=2, framealpha=0.50)
 Fig3.set_xlabel('Density, $\\rho\sigma^3$', fontsize=FS)
@@ -133,13 +129,13 @@ Fig3.set_ylabel('Pressure', fontsize=FS)
 Fig4.set_ylabel('Compressibility', fontsize=FS)
 Fig4.set_yscale('log')
 
-Fig3.plot(rho_converged_Liq, Pressures_Liq, 'b-s', label='Virial')
+Fig3.plot(rho_converged, Pressures, 'b-s', label='Virial')
 Fig3.text(0.10, 0.70, "B$_2$=" + str(np.round(B2,2)), transform=Fig3.transAxes, fontsize=FS)
-Fig3.plot(rho_converged_Liq, P_gas, 'g-d', label='IG + B2')
-Fig3.plot(rho_converged_Liq[1:], P_compressibility, 'r-d', label='Compressibility')
+Fig3.plot(rho_converged, P_gas, 'g-d', label='IG + B2')
+Fig3.plot(rho_converged[1:], P_compressibility, 'r-d', label='Compressibility')
 Fig3.legend(loc='upper left')
 
-Fig4.plot(rho_converged_Liq, Compressibility_Liq, 'r-d')
+Fig4.plot(rho_converged, Compressibility, 'r-d')
 Fig.tight_layout()
 
-Figs2.plot(rho_converged_Liq,ExcessEntropy_Liq, 'r-d')
+Figs2.plot(rho_converged,ExcessEntropy, 'r-d')
